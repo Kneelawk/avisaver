95944ed0eb93f91c7d609b7fb327c223bdaaf331
Cyan Kneelawk <kneelawk@gmail.com>
2026-03-17 18:36:17 -0700

    Setup zeroconf server

--- modules/iced-ui/src/main.rs
+++ modules/iced-ui/src/main.rs
@@ -1,8 +1,20 @@
-use std::thread;
+#[macro_use]
+extern crate tracing;
+
 use std::time::Duration;
+use tokio::time;
 
-fn main() {
-    let _server = avisaver_osc::zeroconf::ZeroconfServer::start(25569).unwrap();
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
 
-    thread::sleep(Duration::from_secs(10));
+    info!("Starting OSC server...");
+
+    let server = avisaver_osc::zeroconf::ZeroconfServer::start(25569).unwrap();
+
+    time::sleep(Duration::from_secs(10)).await;
+
+    info!("Stopping OSC server...");
+
+    server.stop().await;
 }
--- modules/osc/src/zeroconf.rs
+++ modules/osc/src/zeroconf.rs
@@ -4,22 +4,28 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 use std::time::Duration;
+use tokio::sync::Notify;
 use zeroconf::prelude::*;
 use zeroconf::{MdnsService, ServiceRegistration, ServiceType};
 
 pub struct ZeroconfServer {
     running: Arc<AtomicBool>,
+    shutdown: Arc<Notify>,
 }
 
 impl ZeroconfServer {
     pub fn start(osc_port: u16) -> Result<ZeroconfServer, OscError> {
+        info!("Starting Zeroconf server for port {}", osc_port);
         let mut service = MdnsService::new(ServiceType::new("oscjson", "tcp")?, osc_port);
         service.set_name("avisaver");
         service.set_registered_callback(Box::new(zeroconf_service_register));
         let event_loop = service.register()?;
         let running = Arc::new(AtomicBool::new(true));
         let running1 = running.clone();
+        let shutdown = Arc::new(Notify::new());
+        let shutdown1 = shutdown.clone();
         thread::spawn(move || {
+            info!("Zeroconf server started.");
             let _ = &service;
             while running1.load(Ordering::Acquire) {
                 match event_loop.poll(Duration::from_secs(1)) {
@@ -29,14 +35,25 @@ impl ZeroconfServer {
                     }
                 }
             }
+            info!("Zeroconf server stopped.");
+            shutdown1.notify_one();
         });
-        Ok(ZeroconfServer { running })
+        Ok(ZeroconfServer { running, shutdown })
+    }
+
+    pub async fn stop(&self) {
+        if self.running.swap(false, Ordering::AcqRel) {
+            info!("Stopping Zeroconf server...");
+            self.shutdown.notified().await;
+        }
     }
 }
 
 impl Drop for ZeroconfServer {
     fn drop(&mut self) {
-        self.running.store(false, Ordering::Release);
+        if self.running.swap(false, Ordering::AcqRel) {
+            info!("Stopping Zeroconf server...");
+        }
     }
 }
 
